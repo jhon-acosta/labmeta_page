14552039b3b050bdede0f3b7787b5248e19e728c
Acosta-Jhon <jhonerick2018@gmail.com>
2020-04-09 21:56:37 -0500

    structure header

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 
+import Labmeta from '../Labmeta.jpeg'
+
 class Header extends Component {
   render() {
-    return(
+    return (
       <div>
-          <div className="w-full h-8 bg-green-500">
+        <div className="w-full h-8 bg-green-400">
+        </div>
 
+        <div className="w-full h-24 bg-yellow-400">
+          <div className="float-left pl-24">
+            <img src={Labmeta} alt="/" className="w-28 h-24 inset-x-10" />
           </div>
-          <div className="w-full h-8 bg-yellow-500">
-            <h1>HEADER</h1>
+          <div className="float-right">
+            <div className="float-left pr-32">
+              <h1 className="font-extrabold text-3xl">Llama hoy</h1>
+              <p>(+593) 987654321 <br /> (02) 321-456</p>
+            </div>
+            <div className="float-right pr-48">
+              <h1 className="font-extrabold text-3xl">Horario de Apertura</h1>
+              <p>Lun–Vie: 07:00 am–16:00pm <br /> Sab: 07:00am–16:00pm</p>
+            </div>
           </div>
+        </div>
       </div>
     )
   }
