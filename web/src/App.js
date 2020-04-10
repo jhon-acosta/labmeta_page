3a736039906d6c4d8707b478e2a29158bda50dad
Johao Perlaza <perlazajohao@gmail.com>
2020-04-09 20:32:55 -0500

    estructura pagina web

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="lg:bg-red-500 md:bg-green-400 sm:bg-black h-screen">
-      <h1 className="lg:text-6xl text-sm text-yellow-500 text-center">Hello!!!!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div className="lg:bg-red-500 md:bg-green-400 sm:bg-black h-screen">
+        <Header />
+        <h1 className="lg:text-6xl text-sm text-yellow-500 text-center">Hello!!!!</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ web/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return(
+      <div>
+          <div className="w-full h-8 bg-green-500">
+
+          </div>
+          <div className="w-full h-8 bg-yellow-500">
+            <h1>HEADER</h1>
+          </div>
+      </div>
+    )
+  }
+}
+
+export default Header;
