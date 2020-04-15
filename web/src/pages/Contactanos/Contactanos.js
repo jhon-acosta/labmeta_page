404f8edef370c41c2b666fe2bf093089530225ac
Acosta-Jhon <jhonerick2018@gmail.com>
2020-04-14 20:30:00 -0500

    contactanos-icons

--- web/src/pages/Contactanos/Contactanos.js
+++ web/src/pages/Contactanos/Contactanos.js
@@ -12,8 +12,27 @@ class Contactanos extends Component {
     return (
       <div>
         <Header />
-        <div className="bg-gray-500 h-64 w-full"></div>
+        <div className="bg-gray-300 h-64 w-full">
+          <h1 className="text-center lg:text-4xl md:text-4xl text-2xl font-bold text-green-700">Contáctanos</h1>
+          <div className="float-left pl-64 pt-8">
+            <h1 className="text-center lg:text-2xl pb-10 md:text-4xl text-2xl font-seminbold text-green-700">Vía Telefónica</h1>
+            <p><i className="fas fa-phone-alt px-1"></i> (02) 2503238 - 2503037</p>
+            <p><i className="fas fa-mobile-alt px-1"></i> (+593) 996031288 </p>
+
+          </div>
+          <div className="float-right pr-64 pt-8">
+            <h1 className="text-center lg:text-2xl pb-10 md:text-4xl text-2xl font-seminbold text-green-700">Vía E-mail</h1>
+            <p><i className="fas fa-envelope-square px-1"></i> rlabmeta@gmail.com</p>
+          </div>
+        </div>
         <div>
+          <div className="text-center bg-gray-300 pb-4">
+            <h1 className="lg:text-2xl pb-6 md:text-4xl text-2xl font-seminbold text-green-700">Ubicados en:</h1>
+            <p><i className="fas fa-map-marker-alt px-1"></i>
+                Av. Hungria N31-91 y Vancouver, diagonal a la gasolinera
+                Petrocomercial
+              </p>
+          </div>
           <Map center={this.LabmetaLocation} zoom={19}>
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
             <Marker position={this.LabmetaLocation}>
